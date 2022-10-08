5a0d8a063b4540983903440e84b35527085d29fc
Pie <pie@piescode.net>
2022-10-07 22:40:11 -0400

    [PieMonsterEater] Update v1.1!
    Updated plug-in to use players' UUIDs instead of usernames for saving coordinates. Also, implemented some aliases to
    commands.

--- SaveCoords/src/net/piescode/SaveCoords/Main.java
+++ SaveCoords/src/net/piescode/SaveCoords/Main.java
@@ -13,7 +13,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.craftbukkit.libs.jline.internal.Log;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -40,15 +39,20 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(command.getName().equals("setCords")) {
+		if(!(sender instanceof Player)) {
+			getLogger().info("You must be a player to save coordinates!");
+			return false;
+		}
+		
+		if(command.getName().equalsIgnoreCase("setCoords")) {
 			if(sender instanceof Player) {
 				Player p = (Player) sender;
 				if(args.length > 0) {
 				
-				Map<String, String> m = players.get(p.getDisplayName());
+				Map<String, String> m = players.get(p.getUniqueId().toString());
 				m.put(args[0], "x: " + p.getLocation().getBlockX() + " y: " + p.getLocation().getBlockY() + " z: " + p.getLocation().getBlockZ());
 				
-				File f = new File(this.getDataFolder(), p.getDisplayName() + "-Cords.yml");
+				File f = new File(this.getDataFolder(), p.getUniqueId().toString() + "-Cords.yml");
 				FileConfiguration playerData = YamlConfiguration.loadConfiguration(f);
 				
 				ConfigurationSection keys = playerData.getConfigurationSection("keys");
@@ -61,28 +65,28 @@ public class Main extends JavaPlugin {
 				}
 				
 				p.sendMessage("Saved location " + args[0] + " to x: " + p.getLocation().getBlockX() + " y: " + p.getLocation().getBlockY() + " z: " + p.getLocation().getBlockZ());
-				return true;
 			} else p.sendMessage("§4 You must give your coords a name!");
 			}
-			} else if(command.getName().equals("listCords")) {
+			return true;
+			} else if(command.getName().equalsIgnoreCase("listCoords")) {
 				if(sender instanceof Player) {
 					Player p = (Player) sender;
 					
-					Map<String, String> m =  players.get(p.getDisplayName());
+					Map<String, String> m =  players.get(p.getUniqueId().toString());
 					
 					if(m.keySet().size() != 0) {
 					for(String key : m.keySet()) {
 						p.sendMessage(key);
 					}
-					return true;
 					} else p.sendMessage("§4You have not noted down any cords!");
 				}
-			} else if(command.getName().equals("seeCords")) {
+				return true;
+			} else if(command.getName().equalsIgnoreCase("seeCoords")) {
 				if(sender instanceof Player) {
 					Player p = (Player) sender;
 					if(args.length > 0) {
 					
-					Map<String, String> m =  players.get(p.getDisplayName());
+					Map<String, String> m =  players.get(p.getUniqueId().toString());
 					
 					if(m.keySet().size() != 0 && m.get(args[0]) != null) {
 					p.sendMessage(args[0] + ": " + m.get(args[0]));
@@ -92,15 +96,15 @@ public class Main extends JavaPlugin {
 				} else p.sendMessage("§4You have to specify the name of the coords!");
 				}
 				return true;
-			} else if(command.getName().equals("removeCords")) {
+			} else if(command.getName().equalsIgnoreCase("removeCoords")) {
 				if(sender instanceof Player) {
 					Player p = (Player) sender;
 					if(args.length > 0) {
 					
-					Map<String, String> m = players.get(p.getDisplayName());
+					Map<String, String> m = players.get(p.getUniqueId().toString());
 					
 					if(m.keySet().size() != 0 && m.get(args[0]) != null) {
-						File f = new File(this.getDataFolder(), p.getDisplayName() + "-Cords.yml");
+						File f = new File(this.getDataFolder(), p.getUniqueId().toString() + "-Cords.yml");
 						YamlConfiguration playerData = YamlConfiguration.loadConfiguration(f);
 						
 						ConfigurationSection keys = playerData.getConfigurationSection("keys");
@@ -122,6 +126,7 @@ public class Main extends JavaPlugin {
 					} else p.sendMessage("No cords with name " + args[0] + " were found!");
 				} else p.sendMessage("§4You have to specify the name of the coords!");
 				}
+				return true;
 			}
 		return false;
 	}
@@ -129,16 +134,16 @@ public class Main extends JavaPlugin {
 	public void setMap(Player p) {
 		Map<String, String> cordList = new HashMap<>();
 		
-		YamlConfiguration playerData = YamlConfiguration.loadConfiguration(new File(this.getDataFolder(), p.getDisplayName() + "-Cords.yml"));
+		YamlConfiguration playerData = YamlConfiguration.loadConfiguration(new File(this.getDataFolder(), p.getUniqueId().toString() + "-Cords.yml"));
 		
 		ConfigurationSection keys = playerData.getConfigurationSection("keys");
 		
 		for(String key : keys.getKeys(true)) {
 			cordList.put(key, playerData.getString("keys." + key));
-			Log.info("key: " + key + "cords: " + playerData.getString("keys." + key));
+			getLogger().info("key: " + key + "cords: " + playerData.getString("keys." + key));
 		}
 		
-		players.put(p.getDisplayName(), cordList);
+		players.put(p.getUniqueId().toString(), cordList);
 	}
 	
 }
